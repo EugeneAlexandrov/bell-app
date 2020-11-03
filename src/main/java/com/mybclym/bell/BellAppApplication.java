22280bf0e77660f89eb73c62cf3d683abadf5de6
EugenAlexandrov <mybclym@gmail.com>
2020-11-03 16:17:10 +0300

    init project

---
+++ src/main/java/com/mybclym/bell/BellAppApplication.java
@@ -0,0 +1,13 @@
+package com.mybclym.bell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BellAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mybclym/bell/BellAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybclym.bell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BellAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
